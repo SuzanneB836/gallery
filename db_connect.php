1b614d469a025ae64bd1df9f5963167240db9611
Suzanne Boon <6035697@mborijnland.nl>
2025-05-29 00:19:04 +0200

    Fix variable name for MySQL connection to improve code clarity

--- db_connect.php
+++ db_connect.php
@@ -5,9 +5,9 @@ $pass = getenv('DB_PASS');
 $dbname = getenv('DB_NAME');
 $port = getenv('DB_PORT') ?: 3306;
 
-$mysqli = new mysqli($host, $user, $pass, $dbname, $port);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
-if ($mysqli->connect_errno) {
-    die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+if ($conn->connect_errno) {
+    die("Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error);
 }
 ?>
